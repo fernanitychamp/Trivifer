97e85295d2de6a5ff749ee27980c0880e092f296
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-25 18:18:49 -0500

    prepado el setup de la app

---
+++ quizzer/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class Quiz(models.Model):
+    title       = models.CharField(max_length=150)
+    text        = models.TextField()
+    created_at  = models.DateTimeField(default=timezone.now)
+    pub_date    = models.DateTimeField(blank=True, null=True)
+    author      = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Quiz'
+        verbose_name_plural = 'Quizes'
+
+    def publish(self):
+        self.pub_date = timezone.now
+        self.save()
+
+    def __str__(self):
+        return self.title
