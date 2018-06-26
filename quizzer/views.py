bbf2b513f640430875a1699620b27ebee760729f
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-26 10:50:17 -0500

    se creo la pagina para mostrar los registros

--- quizzer/views.py
+++ quizzer/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import loader
 
-# Create your views here.
+from .models import Quiz
+
+
+def index(request):
+    latest_quizes_list = Quiz.objects.order_by('-pub_date')[:3]
+    template = loader.get_template('quizzer/index.html')
+    return HttpResponse(template.render(
+        {'latest_quizes_list': latest_quizes_list},
+        request
+    ))
+
+
+def detail(request, slug):
+    try:
+        quiz = Quiz.objects.filter(slug=slug)
+        if ( len(quiz) == 0 ):
+            raise Quiz.DoesNotExist
+    except Quiz.DoesNotExist:
+        raise Http404("The selected quiz could not be found")
+    return render(request, 'quizzer/detail.html', {'quiz': quiz[0]})
